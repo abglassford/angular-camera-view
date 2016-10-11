80a963c886c9f30118e6893de918eeb93a8b9f1d
Alexander Glassford <a.b.glassford@gmail.com>
2016-10-11 15:52:53 -0600

    complete part 1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 (function() {
   'use strict';
 
+  let app = angular.module('cameraApp', [])
+
+  app.filter('asterisk', function () {
+    return function (input){
+      let ast = []
+      for (var i = 0; i < input; i++) {
+        ast.push('*')
+      }
+      return ast.join('')
+    }
+  });
+
+
+
   $('select').material_select();
 
 })();
